86d55ed6de9805f59075c904fea9c7cf25ac5cc5
Hunter Hughes <Hunter.Hughes@vu.com>
2022-04-01 09:08:18 -0500

    Initial Commit

--- counter.py
+++ counter.py
@@ -1,10 +1,31 @@
 
 def countDayOfTheWeek():
     # This first line is provided for you
-    file_name = input("Enter a file name: ")    # end assignment
+    file_name = input("Enter a file name: ")   
+    f = open(file_name, "r")
+    
+    weekCount = {
+      "Thu": 0,
+      "Fri": 0,
+      "Sat": 0
+    }
+
+    for line in f:
+        line = line.strip()
+        if line.startswith("From") and not line.startswith("From:"):
+               dayOfWeek = line.split()[2]
+               if(dayOfWeek == "Thu"):
+                 weekCount["Thu"] += 1
+               elif(dayOfWeek == "Fri"):
+                 weekCount["Fri"] += 1
+               elif(dayOfWeek == "Sat"):
+                 weekCount["Sat"] += 1
+    
+    print(weekCount)
+
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-##countDayOfTheWeek()
+countDayOfTheWeek()
