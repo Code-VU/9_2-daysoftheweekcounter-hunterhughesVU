d06d06ddfb3d94b113ca4e150fd008481bff11a2
Hunter Hughes <Hunter.Hughes@vu.com>
2022-04-01 09:22:44 -0500

    modified code to make dict more modular for any day of week

--- counter.py
+++ counter.py
@@ -1,25 +1,23 @@
 
+from calendar import week
+from this import d
+
+
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")   
     f = open(file_name, "r")
 
-    weekCount = {
-      "Fri": 0,
-      "Thu": 0,
-      "Sat": 0
-    }
+    weekCount = dict()
 
     for line in f:
         line = line.strip()
         if line.startswith("From") and not line.startswith("From:"):
                dayOfWeek = line.split()[2]
-               if(dayOfWeek == "Thu"):
-                 weekCount["Thu"] += 1
-               elif(dayOfWeek == "Fri"):
-                 weekCount["Fri"] += 1
-               elif(dayOfWeek == "Sat"):
-                 weekCount["Sat"] += 1
+               if dayOfWeek not in weekCount:
+                 weekCount[dayOfWeek] = 1
+               else:
+                 weekCount[dayOfWeek] += 1
     
     print(weekCount)
 
